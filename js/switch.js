5bfd94db8896daa1cedcb08b1ccc837a61b44877
manikantjha <manikantcodes@gmail.com>
2023-10-17 13:57:30 +0530

    objects

--- js/switch.js
+++ js/switch.js
@@ -13,3 +13,20 @@ switch (rank) {
   default:
     console.log("No Medal");
 }
+
+// switch (rank) {
+//   case 1:
+//   case 2:
+//     console.log("Gold Medal");
+//     break;
+//   case 3:
+//   case 4:
+//     console.log("Silver Medal");
+//     break;
+//   case 5:
+//   case 6:
+//     console.log("Bronze Medal");
+//     break;
+//   default:
+//     console.log("No Medal");
+// }
