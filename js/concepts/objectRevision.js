68e827e5c769953a49776a6d2e849ab5223117ea
Manikant Jha <manikantcodes@gmail.com>
2023-11-29 15:15:10 +0530

    object revision

--- js/concepts/objectRevision.js
+++ js/concepts/objectRevision.js
@@ -1,27 +1,45 @@
-// Object is a datastructure.
-// Datastructure = Data ko store karne ka tarika.
-// Const se object banake uski properties change karsakte ho, kyu ke objects store by refrence hote hain.
+// // Object is a datastructure.
+// // Datastructure = Data ko store karne ka tarika.
+// // Const se object banake uski properties change karsakte ho, kyu ke objects store by refrence hote hain.
 
-// let name = "Manikant Jha";
-// let rollNo = 4;
-// let address = "Surat";
-// let phone = "7896541230";
+// // let name = "Manikant Jha";
+// // let rollNo = 4;
+// // let address = "Surat";
+// // let phone = "7896541230";
 
-const astudent = {
-  name: "Manikant Jha",
+var astudent = {
+  name: {
+    firstname: "Manikant",
+    lastname: "Jha",
+  },
   math: 50,
   sci: 60,
   avg: function () {
-    const math = 50;
-    const sci = 60;
-    console.log(math + sci / 2);
+    console.log((this.math + this.sci) / 2);
   },
 };
 
-astudent.avg();
-// student.rollNo = 5;
+var bstudent = {
+  name: {
+    firstname: "Manikant",
+    lastname: "Jha",
+  },
+  math: 50,
+  sci: 50,
+};
 
-// const rollNo = 4;
-// rollNo = 5;
+// // var a = 10;
+// // var b = 20;
+// // function hello() {
+// //   console.log(a + b);
+// // }
+// // hello();
+// astudent.avg();
 
-// console.log("student", student);
+// // student.math = 55;
+// // student["math"] = 55;
+
+// // console.log(student);
+
+// const avg = bstudent.avg.bind(bstudent);
+// avg();
