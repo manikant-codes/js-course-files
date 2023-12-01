ffa97e19bf892df4ac6abbdce20d1b047d6c42de
Manikant Jha <manikantcodes@gmail.com>
2023-12-01 10:18:52 +0530

    classes revision

--- js/concepts/objectRevision.js
+++ js/concepts/objectRevision.js
@@ -7,26 +7,26 @@
 // // let address = "Surat";
 // // let phone = "7896541230";
 
-var astudent = {
-  name: {
-    firstname: "Manikant",
-    lastname: "Jha",
-  },
-  math: 50,
-  sci: 60,
-  avg: function () {
-    console.log((this.math + this.sci) / 2);
-  },
-};
+// var astudent = {
+//   name: {
+//     firstname: "Manikant",
+//     lastname: "Jha",
+//   },
+//   math: 50,
+//   sci: 60,
+//   avg: function () {
+//     console.log((this.math + this.sci) / 2);
+//   },
+// };
 
-var bstudent = {
-  name: {
-    firstname: "Manikant",
-    lastname: "Jha",
-  },
-  math: 50,
-  sci: 50,
-};
+// var bstudent = {
+//   name: {
+//     firstname: "Manikant",
+//     lastname: "Jha",
+//   },
+//   math: 50,
+//   sci: 50,
+// };
 
 // // var a = 10;
 // // var b = 20;
