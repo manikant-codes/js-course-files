dec1d6e6d01dc9046865b3840a823b1f4db5b1d0
Manikant Jha <manikantcodes@gmail.com>
2023-12-01 10:46:05 +0530

    loops revision

---
+++ js/concepts/loopsRevision.js
@@ -0,0 +1,28 @@
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+
+// while
+// do...while
+// for
+// for...of
+// for...in
+
+// let count = 5;
+
+// while(count <= 5) {
+//     console.log(count);
+//     count++;
+// }
+
+// do {
+//     console.log(count);
+//     count++;
+// } while(count <= 5);
+
+
