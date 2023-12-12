b7c14f44049c4e9782b39ffbcf0876068b83bfc6
Manikant Jha <manikantcodes@gmail.com>
2023-12-12 10:41:46 +0530

    array revision

--- js/concepts/arrays/methods/arrayReduce.js
+++ js/concepts/arrays/methods/arrayReduce.js
@@ -59,31 +59,3 @@ const users = [
 //   }, []);
 
 // console.log("res", res);
-
-const b = 10;
-
-const drives = [5, 8, 12];
-const keyboards = [40, 50, 60];
-
-function response() {
-  let max = 0;
-  let keyboard = "";
-  let drive = "";
-  for (const d of drives) {
-    for (const k of keyboards) {
-      if (d + k > max && d + k < b) {
-        max = d + k;
-        keyboard = `${k} Keyboard`;
-        drive = `${d} Drive`;
-      }
-    }
-  }
-
-  if (!max) {
-    return -1;
-  }
-
-  return `Max: ${max}, ${keyboard}, ${drive}`;
-}
-
-console.log(response());
