0b57a9e3adc432f12c21dcf66730025332af2354
Manikant Jha <manikantcodes@gmail.com>
2024-01-05 15:44:18 +0530

    classes

--- js/concepts/objects/constructors.js
+++ js/concepts/objects/constructors.js
@@ -35,26 +35,129 @@
 
 // solution
 
-function Student(rollNo, fname, lname, js, react) {
-  this.rollNo = rollNo;
-  this.fname = fname;
-  this.lname = lname;
-  this.js = js;
-  this.react = react;
-  //   this.getAvg = function () {
-  //     return (this.js + this.react) / 2;
-  //   };
-}
-
 // Student.newProperty = "Something";
 // Student.newMethod = () => {};
-
-Student.prototype.getAvg = function () {
-  return (this.js + this.react) / 2;
-};
-
 // new Object()
 
-const s1 = new Student(1, "Akshay", "Kumar", 42, 43);
-const s2 = new Student(2, "Anil", "Kapoor", 44, 39);
-const s3 = new Student(3, "Hansaben", "Parmar", 48, 47);
+// Student.prototype.getAvg = function () {
+//   return (this.js + this.react) / 2;
+// };
+
+// function Student(rollNo, fname, lname, js, react) {
+//   console.log("this", this);
+//   this.rollNo = rollNo;
+//   this.fname = fname;
+//   this.lname = lname;
+//   this.js = js;
+//   this.react = react;
+//   this.newPoperty = newProperty;
+//   this.getAvg = function () {
+//     return (this.js + this.react) / 2;
+//   };
+// }
+
+// Student.newPoperty = "something";
+
+// Student.prototype.newProperty = "something";
+
+// const s1 = new Student(1, "Akshay", "Kumar", 42, 43);
+// const s2 = new Student(2, "Anil", "Kapoor", 44, 39);
+// const s3 = new Student(3, "Hansaben", "Parmar", 48, 47);
+
+// console.log(s1);
+// console.log(s2);
+// console.log(s3);
+
+// classes
+
+// class Student {
+//   constructor(rollNo, fname, lname, newMethod = () => {}) {
+//     this.rollNo = rollNo;
+//     this.fname = fname;
+//     this.lname = lname;
+//     this.newMethod = newMethod;
+//   }
+
+//   getFullName() {
+//     console.log(this.fname + " " + this.lname);
+//   }
+// }
+
+// Student.prototype.age = 10;
+
+// const s1 = new Student(1, "Akshay", "Kumar");
+// const s2 = new Student(2, "Anil", "Kapoor");
+// const s3 = new Student(3, "Amitabh", "Bachchan");
+
+// console.log(Student.prototype.getFullName.call(s1));
+
+// console.log(s1);
+// console.log(s2);
+// console.log(s3);
+
+// classes inheritance
+
+// debugger;
+
+// class Animal {
+//   constructor(species) {
+//     this.species = species;
+//   }
+
+//   eat() {
+//     console.log("Eating!");
+//   }
+
+//   move() {
+//     console.log("Moving!");
+//   }
+// }
+
+// class Dog extends Animal {
+//   constructor(species, name, color) {
+//     super(species);
+//     this.name = name;
+//     this.color = color;
+//   }
+
+//   get petName() {
+//     // this._petName = this.name + " the " + this.species;
+//     return this._petName;
+//   }
+
+//   set petName(petName) {
+//     this._petName = petName;
+//   }
+
+//   bark() {
+//     console.log("Barking!");
+//   }
+
+//   bite() {
+//     console.log("Biting!");
+//   }
+
+//   play() {
+//     console.log("Playing!");
+//   }
+// }
+
+// const d1 = new Dog("pug", "sheru", "beige");
+// const d2 = new Dog("german shephard", "bruno", "black");
+
+// console.log(d1);
+// console.log(d2);
+
+// static keyword
+
+class TaskManager {
+  constructor(todos) {
+    this.todos = todos;
+  }
+
+  static getDate() {
+    return new Date();
+  }
+}
+
+const t1 = new TaskManager(["a", "b"]);
