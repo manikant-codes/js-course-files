2044ed7fe2d22325d9158fe1cd5434c892ef70c2
Manikant Jha <manikantcodes@gmail.com>
2024-01-12 15:44:13 +0530

    dom

---
+++ notes/dom.js
@@ -0,0 +1,11 @@
+// Document Object Model
+// DOM ek standarization hai, jo HTML documents ko JS objects me convert karta hai.
+
+// Ye method aapko document me se koi bhi element ID ke upar se access karne deta hai.
+// document.getElementById("")
+
+// Ye method aapko document me se tag name ke upar se elements access karne deta hai.
+// document.getElementsByTagName("")
+
+// Ye method aapko document me se class name ke upar se elements access karne deta hai.
+// document.getElementsByClassName("")
