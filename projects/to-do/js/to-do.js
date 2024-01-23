7f3b1f7b179eafc80825bc6e6f7e65259ebae75c
Manikant Jha <manikantcodes@gmail.com>
2024-01-23 15:39:39 +0530

    to-do code refactoring

--- projects/to-do/js/to-do.js
+++ projects/to-do/js/to-do.js
@@ -1,10 +1,32 @@
 let tasks = [];
 let backupTasks = [];
 
+function Task(task, isCompleted, priority) {
+  this.id = new Date();
+  this.task = task;
+  this.isCompleted = isCompleted;
+  this.priority = priority;
+}
+
+function loadTasksList() {
+  const tasksList = document.getElementById("tasks-list");
+  tasksList.innerHTML = "";
+
+  tasks.sort((a, b) => {
+    if (a.priority > b.priority) return 1;
+    if (a.priority === b.priority) return 0;
+    return -1;
+  });
+
+  tasks.forEach((task) => {
+    createAndAddTaskToList(task);
+  });
+}
+
 window.onload = function () {
   try {
     // Get tasks from localstorage;
-    tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks = JSON.parse(localStorage.getItem("tasks"));
   } catch (err) {
     tasks = [];
   }
@@ -13,61 +35,41 @@ window.onload = function () {
   loadTasksList();
 };
 
-function loadTasksList() {
-  const sortedList = tasks.sort((a, b) => {
-    if (a.priority > b.priority) return 1;
-    if (a.priority === b.priority) return 0;
-    return -1;
-  });
-  const tasksList = document.getElementById("tasks-list");
-  tasksList.innerHTML = "";
-  sortedList.forEach((task) => {
-    createTask(task);
-  });
-}
-
 function updateTasksInLocalStorage(tasks) {
   localStorage.setItem("tasks", JSON.stringify(tasks));
   loadTasksList();
 }
 
-function toggleError(isHidden) {
+function showError(isVisible) {
   const errorPara = document.getElementById("error-message");
-  if (isHidden) {
-    errorPara.style.display = "none";
-    errorPara.innerHTML = "";
-  } else {
+  if (isVisible) {
     errorPara.style.display = "block";
     errorPara.innerHTML = "* Task cannot be empty!";
+  } else {
+    errorPara.style.display = "none";
+    errorPara.innerHTML = "";
   }
-}
-
-function Task(task, isCompleted, priority) {
-  this.id = new Date();
-  this.task = task;
-  this.isCompleted = isCompleted;
-  this.priority = priority;
 }
 
 // Create Task
 
 function handleDelete(id, tasksList, taskItem) {
-  tasks = tasks.filter((t) => {
+  tasks = tasks.filter((task) => {
     // Jo id mila hai us id wale task ko list se nikal rahe hain;
-    return t.id !== id;
+    return task.id !== id;
   });
-  tasksList.removeChild(taskItem);
+  // tasksList.removeChild(taskItem);
   // Local storage me naye tasks ki list wapas set kar rahe hain;
   updateTasksInLocalStorage(tasks);
 }
 
 function handleChange(id, para) {
-  tasks = tasks.map((t) => {
-    if (t.id === id) {
+  tasks = tasks.map((task) => {
+    if (task.id === id) {
       // Jo id mila hai us task ka isCompleted change kar rahe hain;
-      return { ...t, isCompleted: !t.isCompleted };
+      return { ...task, isCompleted: !task.isCompleted };
     } else {
-      return t;
+      return task;
     }
   });
   para.classList.toggle("checked");
@@ -110,6 +112,8 @@ function setPriority(priority, optionA, optionB, optionC, optionD) {
   }
 }
 
+// Create Elements
+
 function createElement(tag, children) {
   const element = document.createElement(tag);
 
@@ -125,23 +129,27 @@ function createElement(tag, children) {
   return element;
 }
 
-function createTask(task) {
-  // Para
+function createAndGetPara(task) {
   const para = createElement("p", task.task);
   if (task.isCompleted) {
     para.classList.add("checked");
   } else {
     para.classList.remove("checked");
   }
+  return para;
+}
 
-  // Button
+function createAndGetButton(task) {
   const button = createElement("button");
   button.innerHTML = `<i class="fa-solid fa-trash"></i>`;
   button.onclick = function () {
-    handleDelete(task.id, tasksList, div);
+    handleDelete(task.id);
   };
 
-  // Checkbox
+  return button;
+}
+
+function createAndGetCheckbox(task, para) {
   const input = createElement("input");
   input.setAttribute("type", "checkbox");
   input.checked = task.isCompleted;
@@ -149,61 +157,73 @@ function createTask(task) {
     handleChange(task.id, para);
   };
 
-  // Select
-  const optionA = createElement("option", "A");
-  optionA.value = "A";
-  optionA.style.backgroundColor = "#ff3838";
-  const optionB = createElement("option", "B");
-  optionB.value = "B";
-  optionB.style.backgroundColor = "#fce83a";
-  const optionC = createElement("option", "C");
-  optionC.value = "C";
-  optionC.style.backgroundColor = "#56f000";
-  const optionD = createElement("option", "D");
-  optionD.value = "D";
-  optionD.style.backgroundColor = "#2dccff";
+  return input;
+}
 
-  setPriority(task.priority, optionA, optionB, optionC, optionD);
+function createAndGetSelectDiv(task) {
+  const arr = ["A", "B", "C", "D"];
+  const options = [];
 
-  const select = createElement("select", [optionA, optionB, optionC, optionD]);
+  for (let i = 0; i < arr.length; i++) {
+    const option = createElement("option", arr[i]);
+    option.value = arr[i];
+    option.style.backgroundColor = getBackgroundColor(arr[i]);
+    options.push(option);
+  }
+
+  setPriority.apply(null, [task.priority, ...options]);
+
+  const select = createElement("select", options);
   select.classList.add("priority");
   select.name = "priority";
   select.style.backgroundColor = getBackgroundColor(task.priority);
+
   select.onchange = function (e) {
-    setPriority(e.target.value, optionA, optionB, optionC, optionD);
+    setPriority.apply(null, [e.target.value, ...options]);
     handleSelect(task.id, e.target.value);
   };
+
   const selectDiv = createElement("div", [select]);
   selectDiv.classList.add("custom-select");
 
-  // Div
-  const div = createElement("div", [input, para, selectDiv, button]);
+  return selectDiv;
+}
 
-  const tasksList = document.getElementById("tasks-list");
-  tasksList.appendChild(div);
+// End Create Elements
+
+function createAndAddTaskToList(task) {
+  const para = createAndGetPara(task);
+  const checkbox = createAndGetCheckbox(task, para);
+  const selectDiv = createAndGetSelectDiv(task);
+  const button = createAndGetButton(task);
+  const listItemDiv = createElement("div", [checkbox, para, selectDiv, button]);
+  const tasksListDiv = document.getElementById("tasks-list");
+  tasksListDiv.appendChild(listItemDiv);
 }
 
 // End Create Task
 
 function addTask() {
   const searchInput = document.getElementById("search-input");
+  const task = searchInput.value;
+
   const selectInput = document.getElementById("priority");
   const priority = selectInput.value;
 
-  const task = searchInput.value;
-
   if (task.trim() !== "") {
-    toggleError(true);
-    const objTask = new Task(task, false, priority);
-    tasks.push(objTask);
-    createTask(objTask);
+    showError(false);
+    const newTask = new Task(task, false, priority);
+    tasks.push(newTask);
+    createAndAddTaskToList(newTask);
     updateTasksInLocalStorage(tasks);
     searchInput.value = "";
     selectInput.value = "A";
   } else {
-    toggleError(false);
+    showError(true);
   }
 }
+
+// Search
 
 function toggleSearchBar() {
   const div = document.getElementById("filter-container");
@@ -217,7 +237,6 @@ function search() {
   const filterPriority = filterSelect.value;
   if (filterText) {
     const filteredTasks = tasks.filter((task) => {
-      console.log(task);
       return task.task.includes(filterText);
     });
     if (tasks.length && !backupTasks.length) {
@@ -242,3 +261,5 @@ function search() {
   }
   loadTasksList();
 }
+
+// End Search
