202b8823c2bb3a0bbf9cbb85fc5aaf27842c0fca
Manikant Jha <manikantcodes@gmail.com>
2024-02-09 11:39:37 +0530

    conditionals

---
+++ js/conditionals/ifElse.js
@@ -0,0 +1,20 @@
+const marks = 25;
+
+// if (marks > 25) {
+//   console.log("PASS");
+// } else if (marks === 25) {
+//   console.log("GRACE");
+// } else {
+//   console.log("FAIL");
+// }
+
+// const x = marks > 10 ? "Pass" : "Fail";
+// console.log("x", x);
+
+// let name = 0;
+// let text = "hello";
+// let result = name ?? text;
+
+let str = null;
+
+const x = str?.toUpperCase();
---
+++ js/conditionals/switchCase.js
@@ -0,0 +1,33 @@
+// if (marks > 25) {
+//   console.log("PASS");
+// } else if (marks === 25) {
+//   console.log("GRACE");
+// } else {
+//   console.log("FAIL");
+// }
+
+// const rank = 3;
+
+// if (rank == 1) {
+//   console.log("Gold Medal");
+// } else if (rank == 2) {
+//   console.log("Silver Medal");
+// } else if (rank == 3) {
+//   console.log("Bronze Medal");
+// } else {
+//   console.log("No Medal");
+// }
+
+switch (rank) {
+  case 1:
+    console.log("Gold Medal");
+    break;
+  case 2:
+    console.log("Gold Medal");
+    break;
+  case 3:
+    console.log("Bronze Medal");
+    break;
+  default:
+    console.log("No Medal");
+}
