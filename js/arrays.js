20c312874bb2d5a0288abc315e580f6f93722551
Manikant Jha <manikantcodes@gmail.com>
2024-02-21 11:44:37 +0530

    loops and arrays

---
+++ js/arrays.js
@@ -0,0 +1,24 @@
+// const arr = [2, "hello", false, { msg: "hello" }, [1, 3, 5]];
+const obj = { 0: 2, 1: 4, 2: 6, 3: 8 };
+const arr = [2, 4, 6, 8, 10];
+
+// const arr2 = new Array(4);
+// arr[3] = 4;
+// arr[4] = 8;
+// arr[0] = 100;
+
+// console.log("arr", arr);
+// console.log("obj", obj);
+
+// console.log("arr", arr[0]);
+// console.log("obj", obj[0]);
+
+// for (let index = 0; index < arr.length; index++) {
+//   console.log(arr[index]);
+// }
+
+// for (const value of arr) {
+//   console.log(value);
+// }
+
+// console.log(arr[arr.length - 1]);
--- js/loops.js
+++ js/loops.js
@@ -20,9 +20,28 @@
 //   count++;
 // }
 
-for (let count = 1; count <= 10; count++) {
-  if (count === 5) {
-    continue;
-  }
-  console.log(`2 x ${count} = ${2 * count}`);
-}
+// for (let count = 1; count <= 10; count++) {
+//   if (count === 5) {
+//     continue;
+//   }
+//   console.log(`2 x ${count} = ${2 * count}`);
+// }
+
+// for in
+const person = { name: "John", age: 30, city: "New York" };
+
+// const key = "name";
+// console.log(person.name);
+// console.log(person["name"]);
+// console.log(person[key]);
+
+// for (const key in person) {
+//   console.log(key, person[key]);
+// }
+
+// for of
+// const arr = ["John", 30, "New York"];
+
+// for (const value of arr) {
+//   console.log(value);
+// }
