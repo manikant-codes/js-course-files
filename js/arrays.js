1837c0e0724929039020a1dc8b0daf0ba1d4ef93
Manikant Jha <manikantcodes@gmail.com>
2024-02-27 11:59:35 +0530

    array sorting

--- js/arrays.js
+++ js/arrays.js
@@ -1,5 +1,5 @@
 // const arr = [2, "hello", false, { msg: "hello" }, [1, 3, 5]];
-const obj = { name: 2, 1: 4, 2: 6, 3: 8 };
+// const obj = { name: 2, 1: 4, 2: 6, 3: 8 };
 // const arr = [2];
 // const arr2 = new Array(2);
 
@@ -30,3 +30,27 @@ const obj = { name: 2, 1: 4, 2: 6, 3: 8 };
 // console.log(arr[arr.length - 1]);
 
 // let fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// let arr = ["Banana", "Orange", "Apple", "Mango"];
+
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+// const n3 = 10;
+
+// const r1 = add(n3, 5);
+// const r2 = add(8, 9);
+
+// console.log("r1", r1);
+// console.log("r2", r2);
+
+let arr = [1, 2, 11, 3, 23, 5];
+
+console.log("before sorting", arr);
+
+arr.sort(function (a, b) {
+  return b - a;
+});
+
+console.log("after sorting", arr);
