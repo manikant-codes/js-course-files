41c0664036139228f847625b07db5b3e4336d99f
Manikant Jha <manikantcodes@gmail.com>
2024-03-28 11:57:53 +0530

    json and modules

---
+++ projects/js-modules/index.mjs
@@ -0,0 +1,8 @@
+import { add, subtract } from "./calc.mjs";
+import value from "./calc.mjs";
+
+const sum = add(2, 4);
+console.log("sum", sum);
+
+const difference = subtract(2, 4);
+console.log("difference", difference);
