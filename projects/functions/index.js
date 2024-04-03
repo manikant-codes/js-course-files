6003296083c1c3f38d9c5143538b3f2b2ceadcf4
Manikant Jha <manikantcodes@gmail.com>
2024-04-03 11:52:56 +0530

    taught pass by value and reference, rest parameters, call method

--- projects/functions/index.js
+++ projects/functions/index.js
@@ -51,13 +51,73 @@ function execute() {
 
 // console.log("result", result);
 
-function add(n1 = 0, n2 = 0) {
-  if (typeof n1 !== "number" || typeof n2 !== "number") {
-    return 0;
-  }
-  return n1 + n2;
-}
+// function add(n1 = 0, n2 = 0) {
+//   if (typeof n1 !== "number" || typeof n2 !== "number") {
+//     return 0;
+//   }
+//   return n1 + n2;
+// }
 
-const result = add(1);
+// const result = add(1);
 
-console.log("result", result);
+// console.log("result", result);
+
+// function add(...params) {
+//   let sum = 0;
+//   console.log("arguments", arguments);
+//   for (const value of params) {
+//     sum += value;
+//   }
+
+//   console.log(sum);
+// }
+
+// add(5, 5);
+
+// let num = 10;
+
+// function change(value) {
+//   value = 15;
+//   console.log("value", value); // 15
+// }
+// change(num);
+// console.log("num", num); // 10
+
+// debugger;
+
+// const obj = {
+//   num: 10,
+// };
+
+// function change(obj2) {
+//   obj2.num = 15;
+//   console.log("obj2.num", obj2.num); // 15
+// }
+
+// change(obj);
+// console.log("obj.num", obj.num); // 15
+
+// call, apply and bind
+
+const person = {
+  fname: "John",
+  lname: "Doe",
+  getFullName: function () {
+    console.log(this.fname + " " + this.lname);
+  },
+};
+
+const person2 = {
+  fname: "James",
+  lname: "Doe",
+};
+
+const person3 = {
+  fname: "Marry",
+  lname: "Doe",
+};
+
+person.getFullName();
+
+person.getFullName.call(person2);
+person.getFullName.call(person3);
