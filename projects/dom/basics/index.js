61e7dee8c053779b5ff182eb1bb7b153f150cc50
Manikant Jha <manikantcodes@gmail.com>
2024-04-24 12:09:41 +0530

    taught dom animation

--- projects/dom/basics/index.js
+++ projects/dom/basics/index.js
@@ -6,20 +6,20 @@
 // document.getElementsByTagName("p")[2].innerHTML = "Hello JavaScript";
 // document.getElementsByTagName("p")[3].innerHTML = "Hello JavaScript";
 
-const paragraphs = document.getElementsByTagName("p");
-const paras = document.getElementsByClassName("para");
+// const paragraphs = document.getElementsByTagName("p");
+// const paras = document.getElementsByClassName("para");
 
-let index = 1;
+// let index = 1;
 
-for (const p of paragraphs) {
-  p.innerHTML = "Hello JS";
-  // p.className = "para-" + index;
-  p.onclick = function () {
-    alert(`You clicked on the paragraph`);
-  };
-  // p.setAttribute("onclick", "alert('Hello!')");
-  index++;
-}
+// for (const p of paragraphs) {
+//   p.innerHTML = "Hello JS";
+//   // p.className = "para-" + index;
+//   p.onclick = function () {
+//     alert(`You clicked on the paragraph`);
+//   };
+//   // p.setAttribute("onclick", "alert('Hello!')");
+//   index++;
+// }
 
 // for (const p of paras) {
 //   p.innerHTML = "Hello World!";
@@ -27,3 +27,25 @@ for (const p of paragraphs) {
 //   p.style.color = "white";
 //   p.style.padding = "8px";
 // }
+
+// const paras = document.querySelectorAll("p:last-child");
+
+// console.log("paras", paras);
+
+// document.write("Hello");
+
+const innerDiv = document.getElementById("inner-div");
+
+const intervalID = setInterval(() => {
+  console.log(innerDiv.style.left);
+  if (Number(innerDiv.style.left.split("px")[0]) < 350) {
+    innerDiv.style.left = Number(innerDiv.style.left.split("px")[0]) + 1 + "px";
+    innerDiv.style.top = Number(innerDiv.style.top.split("px")[0]) + 1 + "px";
+  } else {
+    clearInterval(intervalID);
+  }
+}, 0);
+
+// const timeoutID = setTimeout(() => {
+//   clearTimeout(timeoutID);
+// }, 1000);
