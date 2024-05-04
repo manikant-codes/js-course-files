c2340eb34abc9ea79ae2a066a7ba7225248a8567
Manikant Jha <manikantcodes@gmail.com>
2024-05-04 09:49:49 +0530

    taught output

---
+++ js/concepts/output/index.js
@@ -0,0 +1,9 @@
+// const title = document.getElementById("title");
+// title.innerHTML = "New Title";
+
+// document.write("New Content");
+// document.write("<h1>New Content</h1>");
+
+// window.alert("Hello!");
+
+// console.log("42" + 2);
