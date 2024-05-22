b85dcc9c119df0cf4123565122d5f581a946f2d1
Manikant Jha <manikantcodes@gmail.com>
2024-05-22 10:08:17 +0530

    taught functions

---
+++ theory/functions/functions.js
@@ -0,0 +1,41 @@
+function makeTea(pani, milk, chayPatti, chini, adrak) {
+  let x = 10;
+
+  console.log("Boil " + pani + "ml water");
+  console.log("Add " + chayPatti + "spoon chay patti");
+  console.log("Add " + chini + "spoon chini");
+  console.log("Add " + milk + "ml milk");
+  console.log("Add " + adrak + "inch adrak");
+  console.log("Tea is ready: ☕");
+
+  return "☕";
+}
+
+console.log("x", x);
+
+const tea1 = makeTea(250, 250, 2, 0, 1);
+console.log("tea1", tea1);
+makeTea(250, 250, 1, 1, 1); // Function call or function invocation
+makeTea(250, 250, 2, 2, 0);
+makeTea(750, 750, 3, 3, 1.5);
+
+// console.log("Boil " + 250 + "ml water");
+// console.log("Add " + 2 + "spoon chay patti");
+// console.log("Add " + 2 + "spoon chini");
+// console.log("Add " + 250 + "ml milk");
+// // console.log("Add " + 1 + "inch adrak");
+// console.log("Tea is ready: ☕");
+
+// console.log("Boil " + 250 + "ml water");
+// console.log("Add " + 2 + "spoon chay patti");
+// console.log("Add " + 2 + "spoon chini");
+// console.log("Add " + 250 + "ml milk");
+// // console.log("Add " + 1 + "inch adrak");
+// console.log("Tea is ready: ☕");
+
+// console.log("Boil " + 250 + "ml water");
+// console.log("Add " + 2 + "spoon chay patti");
+// console.log("Add " + 2 + "spoon chini");
+// console.log("Add " + 250 + "ml milk");
+// // console.log("Add " + 1 + "inch adrak");
+// console.log("Tea is ready: ☕");
