6dba4564301322d0a6b52f663ccbbfd1db3a78c8
Manikant Jha <manikantcodes@gmail.com>
2024-05-28 10:06:05 +0530

    made reactangle calc

--- projects-II/rectangleCalc/index.js
+++ projects-II/rectangleCalc/index.js
@@ -14,3 +14,25 @@ function handleSelect() {
     outputPara.innerHTML = "A = <span id='result'>2(l+w)</span>";
   }
 }
+
+function calculate() {
+  const calculation = calculationSelect.value;
+  const length = Number(lengthInput.value);
+  const width = Number(widthInput.value);
+  // const length = lengthInput.value;
+  // const width = widthInput.value;
+  const output = document.getElementById("result");
+
+  let result = 0;
+
+  if (calculation === "area") {
+    result = length * width;
+  } else if (calculation === "perimeter") {
+    result = 2 * (length + width);
+  } else {
+    result = (width ** 2 + length ** 2) ** 0.5;
+    result = result.toFixed(2);
+  }
+
+  output.innerText = result;
+}
