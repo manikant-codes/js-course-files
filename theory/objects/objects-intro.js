788fdd392bcf4336da002494dfe842ba52839091
Manikant Jha <manikantcodes@gmail.com>
2024-06-03 10:07:25 +0530

    taught event and objects

--- theory/objects/objects-intro.js
+++ theory/objects/objects-intro.js
@@ -1,20 +1,51 @@
-let student1Name = "Manikant";
-let student1Roll = 10;
-let student1Class = "JS";
-let student1Marks = 70;
+// let student1Name = "Manikant";
+// let student1Roll = 10;
+// let student1Class = "JS";
+// let student1Marks = 70;
 
-let student2 = {
-  name: "Ritu",
+let student = {
+  fname: "Ritu",
+  lname: "Jha",
   roll: 12,
-  class: "React",
-  marks: 32,
-  "fav color": "Blue",
-  "fav-color": "Blue"
+  hobbies: ["Reading", "Singing", "Drawing"],
+  address: {
+    city: "Surat",
+    pin: 394107
+  },
+  createdAt: new Date(),
+  math: 66,
+  physics: 77,
+  chemistry: 88,
+  getPercentage: function () {
+    const percentage =
+      ((this.chemistry + this.physics + this.math) * 100) / 300;
+    return percentage;
+  }
 };
 
-student2.age = 20;
+student.getFullName = function () {
+  console.log(this.fname + " " + this.lname);
+};
 
-console.log("name", student2.name);
-console.log("name", student2["fav color"]);
+const result = student.getPercentage();
 
-// checkIfPass(student2.marks);
+console.log(result + 3);
+
+// let student2 = {
+//   name: "Abhishek",
+//   roll: 12,
+//   math: 56,
+//   physics: 75,
+//   chemistry: 81
+// };
+
+// function getPercentage(student) {
+//   const percentage1 =
+//     ((student.chemistry + student.physics + student.math) * 100) / 300;
+//   console.log(percentage1);
+// }
+
+// getPercentage(student1);
+// getPercentage(student2);
+
+student.getPercentage();
---
+++ theory/objects/objects_display.js
@@ -0,0 +1,75 @@
+// let student = {
+//   fname: "Ritu",
+//   lname: "Jha",
+//   roll: 12,
+//   hobbies: ["Reading", "Singing", "Drawing"],
+//   address: {
+//     city: "Surat",
+//     pin: 394107
+//   },
+//   createdAt: new Date(),
+//   math: 66,
+//   physics: 77,
+//   chemistry: 88,
+//   getPercentage: function () {
+//     const percentage =
+//       ((this.chemistry + this.physics + this.math) * 100) / 300;
+//     return percentage;
+//   }
+// };
+
+// let value = "";
+
+// value += "Fname: " + student.fname + "<br/>";
+// value += "Lname: " + student.lname + "<br/>";
+// value += "Roll: " + student.roll + "<br/>";
+// value += "Hobbies: " + student.hobbies + "<br/>";
+// value += "City: " + student.address.city + "<br/>";
+// value += "Pin: " + student.address.pin + "<br/>";
+// value += "CreatedAt: " + student.createdAt + "<br/>";
+// value += "Math: " + student.math + "<br/>";
+// value += "Physics: " + student.physics + "<br/>";
+// value += "Chemistry: " + student.chemistry + "<br/>";
+
+// console.log("Object.values()", Object.values(student));
+// console.log("Object.keys()", Object.keys(student));
+// console.log("Object.entries()", Object.entries(student));
+
+// console.log("JSON.stringify(student)", JSON.stringify(student));
+
+// document.getElementById("student").innerHTML = JSON.stringify(student);
+
+const fan = {
+  color: "White",
+  company: "Cropton",
+  price: 1500,
+  maxSpeed: 5,
+  minSpeed: 0,
+  isOn: false,
+  currentSpeed: 0,
+  increseSpeed: function () {
+    this.currentSpeed = this.currentSpeed + 1;
+    console.log("Current speed: ", this.currentSpeed);
+  },
+  decreseSpeed: function () {
+    this.currentSpeed = this.currentSpeed - 1;
+    console.log("Current speed: ", this.currentSpeed);
+  },
+  switchOn: function () {
+    this.isOn = true;
+    console.log("Fan is on!");
+  },
+  switchOff: function () {
+    this.isOn = false;
+    console.log("Fan is off!");
+  }
+};
+
+fan.switchOn();
+fan.increseSpeed();
+fan.increseSpeed();
+fan.increseSpeed();
+fan.increseSpeed();
+fan.increseSpeed();
+fan.decreseSpeed();
+fan.switchOff();
