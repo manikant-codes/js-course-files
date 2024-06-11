f1b11b9b3fc10ea54004ee3272864f62a6adb057
Manikant Jha <being.manikantjha@gmail.com>
2024-06-11 19:18:09 +0530

    taught how to get value from select

--- projects-II/simpleCalc/index.js
+++ projects-II/simpleCalc/index.js
@@ -1,18 +1,46 @@
 const input1 = document.getElementById("num1");
 const input2 = document.getElementById("num2");
+const selectCalculation = document.getElementById("calculation");
 const output = document.getElementById("output");
 
-function add() {
-  const num1 = input1.value;
-  const num2 = input2.value;
+function calculate() {
+  const num1 = Number(input1.value);
+  const num2 = Number(input2.value);
 
-  const result = Number(num1) + Number(num2);
-  output.innerText = result;
+  const selection = selectCalculation.value;
+
+  if (selection == "add") {
+    const result = num1 + num2;
+    output.innerText = result;
+  } else if (selection == "subtract") {
+    const result = num1 - num2;
+    output.innerText = result;
+  } else if (selection == "multiply") {
+    const result = num1 * num2;
+    output.innerText = result;
+  } else {
+    const result = num1 / num2;
+    output.innerText = result;
+  }
 }
 
-function subtract() {
-  const num1 = input1.value;
-  const num2 = input2.value;
-  const result = Number(num1) - Number(num2);
-  output.innerText = result;
+function backspace() {
+  input1.value = input1.value.slice(0, input1.value.length - 1);
 }
+
+// window.addEventListener("keydown", backspace);
+
+// function add() {
+//   const num1 = input1.value;
+//   const num2 = input2.value;
+
+//   const result = Number(num1) + Number(num2);
+//   output.innerText = result;
+// }
+
+// function subtract() {
+//   const num1 = input1.value;
+//   const num2 = input2.value;
+//   const result = Number(num1) - Number(num2);
+//   output.innerText = result;
+// }
