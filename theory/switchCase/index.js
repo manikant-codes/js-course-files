93fbe4c4ce20d3179a12bb824989d24b5c95e074
Manikant Jha <manikantcodes@gmail.com>
2024-06-13 10:12:25 +0530

    taught switch case

---
+++ theory/switchCase/index.js
@@ -0,0 +1,58 @@
+// debugger;
+
+// if (marks >= 90) {
+//   console.log("Gold medal!");
+// } else if (marks >= 80) {
+//   console.log("Silver medal!");
+// } else if (marks >= 70) {
+//   console.log("Bronze medal!");
+// } else {
+//   console.log("No medal!");
+// }
+
+// const marks = 90;
+
+// function getGrade() {
+//   if (marks >= 90) {
+//     return "A";
+//   } else if (marks >= 80) {
+//     return "B";
+//   } else if (marks >= 70) {
+//     return "C";
+//   } else {
+//     return "D";
+//   }
+// }
+
+// const grade = getGrade();
+
+// ===
+// >
+// <
+// >=
+// <=
+// !==
+// !=
+// ==
+// &&
+// ||
+// !
+
+debugger;
+
+const rank = 4;
+
+switch (rank) {
+  case 1:
+  case 2:
+  case 3:
+    console.log("Gold medal!");
+    break;
+  case 4:
+  case 5:
+  case 6:
+    console.log("Silver medal!");
+    break;
+  default:
+    console.log("No medal!");
+}
