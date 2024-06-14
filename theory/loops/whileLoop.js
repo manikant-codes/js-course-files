7d5fd3cab676ff4b841a59633f5f3f747e4b4a35
Manikant Jha <manikantcodes@gmail.com>
2024-06-14 10:12:48 +0530

    taught loops

---
+++ theory/loops/whileLoop.js
@@ -0,0 +1,24 @@
+// console.log(1 * 1);
+// console.log(1 * 2);
+// console.log(1 * 3);
+// console.log(1 * 4);
+// console.log(1 * 5);
+// console.log(1 * 6);
+// console.log(1 * 7);
+// console.log(1 * 8);
+// console.log(1 * 9);
+// console.log(1 * 10);
+
+debugger;
+
+let i = 5;
+
+while (i <= 4) {
+  console.log(1 * i);
+  i++;
+}
+
+// do {
+//   console.log(1 * i);
+//   i++;
+// } while (i <= 4);
