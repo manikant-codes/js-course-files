2b4ab1c7e7de080656127b2b84f225c1b9f2327c
Manikant Jha <being.manikantjha@gmail.com>
2024-07-02 10:15:56 +0530

    taught array splice

--- theory/arrays/intro.js
+++ theory/arrays/intro.js
@@ -1,20 +1,20 @@
-const studentObj = { name: "Manikant", roll: 10, math: 45 };
+// const studentObj = { name: "Manikant", roll: 10, math: 45 };
 
-const studentArr = [
-  "Manikant",
-  10,
-  "Surat",
-  true,
-  false,
-  { name: "Manikant" },
-  [1, 2, 3, [2, 4, 6, { 0: [0, 4, 6] }]],
-  function () {},
-  /[a-z0-9]/,
-  new Date(),
-  10n,
-];
+// const studentArr = [
+//   "Manikant",
+//   10,
+//   "Surat",
+//   true,
+//   false,
+//   { name: "Manikant" },
+//   [1, 2, 3, [2, 4, 6, { 0: [0, 4, 6] }]],
+//   function () {},
+//   /[a-z0-9]/,
+//   new Date(),
+//   10n,
+// ];
 
-const arr = new Array(1, 2, 3);
+// const arr = new Array(1, 2, 3);
 
 // console.log(studentObj["name"]);
 
@@ -26,4 +26,64 @@ const arr = new Array(1, 2, 3);
 
 // studentArr.someProperty = "Some Value";
 
-console.log(studentArr);
+// console.log(studentArr);
+
+// const fruits = [
+//   "Apple",
+//   "Banana",
+//   "Cherry",
+//   "Date",
+//   "Elderberry",
+//   "Fig",
+//   "Grape",
+//   "Guava",
+//   "Honeydew",
+//   "Ice Apple",
+//   "Jackfruit",
+//   "Kiwi",
+//   "Lemon",
+//   "Mango",
+//   "Nectarine",
+//   "Orange",
+//   "Papaya",
+//   "Quince",
+//   "Raspberry",
+//   "Strawberry",
+//   "Tangerine",
+//   "Ugli fruit",
+//   "Victoria plum",
+//   "Watermelon",
+//   "Xigua",
+//   "Yuzu",
+//   "Ziziphus",
+// ];
+
+// const fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+
+// fruits.unshift("Mango");
+// fruits.push("Kiwi");
+// fruits[1] = "Pineapple";
+
+// fruits.splice(1, 0, "Pineapple");
+
+// console.log("fruits", fruits);
+
+const fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+
+function mySplice(pos, element) {
+  let temp = [];
+
+  for (let i = 0; i <= fruits.length; i++) {
+    if (i < pos) {
+      temp[i] = fruits[i];
+    } else if (i === pos) {
+      temp[i] = element;
+    } else {
+      temp[i] = fruits[i - 1];
+    }
+  }
+
+  return temp;
+}
+
+const newArr = mySplice(2, "Jamun");
