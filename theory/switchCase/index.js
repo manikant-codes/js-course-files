8a1b6edda7b9674c81a3edbe9c8c01cb73525e21
Manikant Jha <being.manikantjha@gmail.com>
2024-07-08 19:46:07 +0530

    taught switch-case

--- theory/switchCase/index.js
+++ theory/switchCase/index.js
@@ -1,31 +1,25 @@
-const rank = 1;
-
-// If else me sare comparision operators aur logical operators ka use kar sakte hoo.
+const rank = 2;
 
 // debugger;
 
-// if (rank == 1) {
+// if (rank === 1) {
 //   console.log("Gold Medal");
-// } else if (rank == 2) {
+// } else if (rank === 2) {
 //   console.log("Silver Medal");
-// } else if (rank == 3) {
+// } else if (rank === 3) {
 //   console.log("Bronze Medal");
 // } else {
 //   console.log("No Medal");
 // }
 
-// Switch case me sirf === ka comparision hota hai.
 switch (rank) {
   case 1:
+  case 2:
     console.log("Gold Medal");
-    break;
-  case 1:
-    console.log("Silver Medal");
     break;
   case 3:
     console.log("Bronze Medal");
     break;
   default:
     console.log("No Medal");
-    break;
 }
