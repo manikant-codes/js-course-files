627a3efc0ba19e4416d40706cc223ae9c91abff4
Manikant Jha <manikantcodes@gmail.com>
2024-07-09 19:41:13 +0530

    taught while loop

--- theory/loops/whileLoop.js
+++ theory/loops/whileLoop.js
@@ -1,24 +1,43 @@
-// console.log(1 * 1);
-// console.log(1 * 2);
-// console.log(1 * 3);
-// console.log(1 * 4);
-// console.log(1 * 5);
-// console.log(1 * 6);
-// console.log(1 * 7);
-// console.log(1 * 8);
-// console.log(1 * 9);
-// console.log(1 * 10);
+// The Problem
 
-debugger;
+// console.log("4");
+// console.log("4");
+// console.log("4");
+// console.log("4");
+// console.log("4");
 
-let i = 5;
+// console.log("3 x 1 = 3");
+// console.log("3 x 2 = 6");
+// console.log("3 x 3 = 9");
+// console.log("3 x 4 = 12");
+// console.log("3 x 5 = 15");
 
-while (i <= 4) {
-  console.log(1 * i);
-  i++;
+// The Solution
+
+let counter = 1;
+
+// debugger;
+
+// while (counter <= 5) {
+//   console.log(counter);
+//   counter++;
+// }
+
+const tableDiv = document.getElementById("table");
+
+let html = "";
+
+while (counter <= 10) {
+  html =
+    html +
+    "<p class='table-row'>" +
+    5 +
+    " x " +
+    counter +
+    " = " +
+    5 * counter +
+    "</p>";
+  counter++;
 }
 
-// do {
-//   console.log(1 * i);
-//   i++;
-// } while (i <= 4);
+tableDiv.innerHTML = html;
