3ca930f433d1bb121599607e7c7b7ab7759de79d
Manikant Jha <manikantcodes@gmail.com>
2024-07-19 15:21:12 +0530

    initialized new branch

--- theory/arrays/shuffling.js
+++
@@ -1,7 +0,0 @@
-const numbers = [10, 45, 56, 23, 28, 49, 1, 22, 36, 7];
-
-numbers.sort(function (a, b) {
-  return 0.5 - Math.random();
-});
-
-console.log("numbers", numbers);
---
+++ theory/display-output/index.js
@@ -0,0 +1,23 @@
+// window.alert("Hello World!");
+// OR
+// alert("Hello World!");
+
+// window.console.log("Helo World!");
+// OR
+// console.log("Helo World!");
+
+// window.document.write("Helo World!");
+// OR
+// document.write("Helo World!");
+
+// window.document.getElementById("title").innerText = "<span>Hello World!</span>";
+// OR
+// document.getElementById("title").innerText = "<span>Hello World!</span>";
+
+// window.document.getElementById("title").innerHTML =
+//   "<span style='color:red'>Hello World!</span>";
+// OR
+// document.getElementById("title").innerHTML =
+//   "<span style='color:red'>Hello World!</span>";
+
+debugger;
