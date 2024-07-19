79effce10720ae7c6d5dbf595cb934cd1c216201
Manikant Jha <manikantcodes@gmail.com>
2024-07-19 19:48:58 +0530

    arrays intro

---
+++ theory/arrays/index.js
@@ -0,0 +1,61 @@
+const objStudent = {
+  //   name: "John Doe",
+  //   age: 25,
+  //   city: "New York",
+  //   math: 90,
+  //   english: 85,
+  //   science: 95,
+
+  0: "John Doe",
+  1: 25,
+  2: "New York",
+  3: { city: "Surat", pin: 394107 },
+};
+
+// objStudent.math = 90;
+// objStudent.english = 85;
+// objStudent.science = 95;
+objStudent[4] = 90;
+objStudent[5] = 85;
+objStudent[6] = 95;
+
+objStudent[0] = "Manikant Jha";
+
+// console.log(">>> name:", objStudent.name);
+// console.log(">>> name:", objStudent["name"]);
+
+// console.log(">>> name:", objStudent.0); // error
+// console.log(">>> name:", objStudent["0"]);
+// console.log(">>> name:", objStudent[0]);
+
+const arrStudent = ["John Doe", 25, "New York", { city: "Surat", pin: 394107 }];
+arrStudent[4] = 90;
+arrStudent[5] = 85;
+arrStudent[6] = 95;
+
+arrStudent[0] = "Manikant Jha";
+
+// console.log(">>> objStudent:", typeof objStudent);
+// console.log(">>> arrStudent:", typeof arrStudent);
+
+// console.log(">>> objStudent:", objStudent);
+// console.log(">>> arrStudent:", arrStudent);
+
+// console.log(">>> objStudent:", objStudent["0"]);
+// console.log(">>> arrStudent:", arrStudent["0"]);
+
+// console.log(">>> objStudent:", objStudent.toString());
+// console.log(">>> arrStudent:", arrStudent.toString());
+
+// console.log(">>> arrStudent:", arrStudent);
+// console.log(">>> arrStudent:", arrStudent.length);
+
+// console.log(">>> arrStudent:", arrStudent[0]);
+// console.log(">>> arrStudent:", arrStudent[arrStudent.length - 1]);
+
+// arrStudent.push("India");
+// arrStudent.push("India");
+
+arrStudent[arrStudent.length] = "India";
+
+console.log(arrStudent);
