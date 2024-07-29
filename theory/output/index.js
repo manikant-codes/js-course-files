2e3ea371f3e734aa59f2bddb468c9ec75abd4674
Manikant Jha <manikantcodes@gmail.com>
2024-07-29 09:36:06 +0530

    upto variables

---
+++ theory/output/index.js
@@ -0,0 +1,5 @@
+// This shows an alert dialog.
+// alert("Hello world!");
+// document.write("Hello JS!");
+// console.log("Hello world!");
+// console.log(2 + 2);
---
+++ theory/variables/index.js
@@ -0,0 +1,29 @@
+// var a; // Declaration
+// a = 10; // Assignment
+
+// console.log(">>> a", a);
+
+// var a; // Re-Declaration
+// a = 45; // Re-Assignment
+
+// console.log(">>> a", a);
+
+// let a; // Declaration
+// a = 10; // Assignment
+
+// console.log(">>> a", a);
+
+// // let a; // Re-Declaration
+// a = 45; // Re-Assignment
+
+// console.log(">>> a", a);
+
+const a = 10; // Declaration
+// Assignment
+
+console.log(">>> a", a);
+
+// const a; // Re-Declaration
+a = 45; // Re-Assignment
+
+console.log(">>> a", a);
