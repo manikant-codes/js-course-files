89e61fda8ecf2c950a60c559b621f153216f9576
Manikant Jha <manikantcodes@gmail.com>
2024-07-30 15:09:05 +0530

    made simple calc

---
+++ projects/simpleCalc/index.js
@@ -0,0 +1,9 @@
+let num1Input = document.getElementById("num1");
+let num2Input = document.getElementById("num2");
+
+function add() {
+  let num1 = Number(num1Input.value);
+  let num2 = Number(num2Input.value);
+
+  console.log(num1 + num2);
+}
---
+++ theory/arithmaticOperators/index.js
@@ -0,0 +1,8 @@
+let x = 10;
+let y = --x;
+
+1 - 1;
+"1" - "1";
+
+console.log("x", x);
+console.log("y", y);
