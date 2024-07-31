61899ce6edf08bd9ef995659c3ab2bbfdfaebe2b
Manikant Jha <manikantcodes@gmail.com>
2024-07-31 14:06:14 +0530

    string methods

--- theory/strings/index.js
+++ theory/strings/index.js
@@ -1,6 +1,19 @@
+// Escape Characters
 // let name = "Meet said, \"Hello\"";
 // let name = 'Meet said, \'Hello\'';
 // let name = " \"\" ";
 // let name = ' \'\' ';
 // let name = ` \`\` `;
-let name = ` \\ `;
+// let name = ` \\ `;
+
+// Strings as Objects
+let name1 = "John";
+let name2 = "John";
+
+let name3 = new String("John");
+let name4 = new String("John");
+
+console.log(name1 == name2);
+console.log(name1 == name3);
+console.log(name1 == name4);
+console.log(name3 == name4);
---
+++ theory/strings/string-methods.js
@@ -0,0 +1,8 @@
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// console.log("text.length", text.length);
+// console.log("text.charAt(3)", text.charAt(3));
+// console.log("text.at(0)", text.at(0));
+// console.log("text[3]", text[3]);
+// console.log("text.charCodeAt(0)", text.charCodeAt(0));
+
+console.log(text[0]);
