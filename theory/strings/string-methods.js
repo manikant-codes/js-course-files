b76df582d7d9d9869eae405b801c77dd3af15baa
Manikant Jha <manikantcodes@gmail.com>
2024-08-01 10:29:52 +0530

    taught string methods

--- theory/strings/string-methods.js
+++ theory/strings/string-methods.js
@@ -1,8 +1,18 @@
-let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 // console.log("text.length", text.length);
 // console.log("text.charAt(3)", text.charAt(3));
 // console.log("text.at(0)", text.at(0));
 // console.log("text[3]", text[3]);
 // console.log("text.charCodeAt(0)", text.charCodeAt(0));
+// console.log(text[0]);
 
-console.log(text[0]);
+// let text = "Apple, Banana, Kiwi";
+
+// const slice = text.slice(-4);
+// const substring = text.substring(-4);
+// const substr = text.substr(7, 6);
+
+// console.log(slice);
+// console.log(substr);
+
+let text = "Apple, Banana, Kiwi, Banana";
