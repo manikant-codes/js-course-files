f056034560904e9d3b06a6d20499f41aa23b33c0
Manikant Jha <manikantcodes@gmail.com>
2024-08-01 15:13:04 +0530

    taught assignment operators

--- theory/variables/index.js
+++ theory/variables/index.js
@@ -32,8 +32,8 @@ debugger;
 // const se bane variables ki bhi hoisting hooti hai. Lekin unme var ke tarah undefined nahi hoota hai. Is ka matlab hai ke assignment se pehle inki value access nahi karsakte hoo, kyu ke assignment se pehle koi value hi nahi hooti hai. Is value nahi hone ke state ko ham kahenge ke variable abhi temporal dead zone me hai.
 // const se bane variables re-declaration aur re-assignment dono hi nahi hoosakti hai.
 
-const a = 10; // Declartion
-console.log("a", a);
+// const a = 10; // Declartion
+// console.log("a", a);
 
 // const a; // Re-declaration not possible.
-a = 20; // Re-assignment not possible.
+// a = 20; // Re-assignment not possible.
