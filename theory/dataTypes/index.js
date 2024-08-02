651fc8bdf85aa4003c65d0e638ed222213f88648
Manikant Jha <manikantcodes@gmail.com>
2024-08-02 15:10:51 +0530

    functions

---
+++ theory/dataTypes/index.js
@@ -0,0 +1,15 @@
+// console.log(typeof 45);
+// console.log(typeof "Hello");
+// console.log(typeof true);
+// console.log(typeof {});
+// console.log(typeof []);
+// console.log(typeof new Date());
+// console.log(typeof /[a-z]/);
+// console.log(typeof typeof 45);
+// console.log(typeof "number");
+// console.log(typeof NaN);
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+// let z = null;
+// console.log(z);
---
+++ theory/functions/index.js
@@ -0,0 +1,13 @@
+function makeTea(water, sugar, tea, milk, ginger) {
+  console.log("Add " + water + "ml of water.");
+  console.log("Add " + sugar + "tsp of sugar.");
+  console.log("Add " + tea + "tsp of tea.");
+  console.log("Let it boil.");
+  console.log("Add " + milk + "ml of milk.");
+  console.log("Add " + ginger + " inch of ginger.");
+  console.log("Tea is ready ☕.");
+}
+
+// makeTea(125, 2, 2, 125, 1);
+// makeTea(250, 4, 4, 250, 2);
+makeTea(500, 8, 8, 500, 4);
