bcb73e8e1ab9438f6ff7892106a8592dbed43c46
Manikant Jha <manikantcodes@gmail.com>
2024-08-06 15:04:39 +0530

    simple calc project

--- projects/simpleCalc/index.js
+++ projects/simpleCalc/index.js
@@ -1,9 +1,31 @@
 let num1Input = document.getElementById("num1");
 let num2Input = document.getElementById("num2");
+let outputPara = document.getElementById("output");
 
 function add() {
   let num1 = Number(num1Input.value);
   let num2 = Number(num2Input.value);
 
-  console.log(num1 + num2);
+  outputPara.innerText = num1 + num2;
+}
+
+function subtract() {
+  let num1 = num1Input.value;
+  let num2 = num2Input.value;
+
+  outputPara.innerText = num1 - num2;
+}
+
+function multiply() {
+  let num1 = num1Input.value;
+  let num2 = num2Input.value;
+
+  outputPara.innerText = num1 * num2;
+}
+
+function divide() {
+  let num1 = num1Input.value;
+  let num2 = num2Input.value;
+
+  outputPara.innerText = num1 / num2;
 }
