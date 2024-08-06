81cda3b8df891296945775bdb14a7dc5876a8301
Manikant Jha <manikantcodes@gmail.com>
2024-08-06 09:18:09 +0530

    function and objects

---
+++ theory/functions/index.js
@@ -0,0 +1,30 @@
+// function makeTea(water, sugar, tea, milk, ginger) {
+//   console.log("Add " + water + "ml water.");
+//   console.log("Add " + sugar + "tsp sugar.");
+//   console.log("Add " + tea + "tsp tea.");
+//   console.log("Let it boil.");
+//   console.log("Add " + milk + "ml milk.");
+//   console.log("Add " + ginger + "inch ginger.");
+//   console.log("Tea is ready " + "☕".repeat(ginger));
+// }
+
+// makeTea(125, 2, 2, 125, 1);
+// makeTea(250, 4, 4, 250, 2);
+// makeTea(375, 6, 6, 375, 3);
+// makeTea(500, 8, 8, 500, 4);
+
+function calculate(a, b) {
+  console.log(a + b);
+}
+
+calculate(2, 2);
+calculate(14, 4);
+calculate(3, 1);
+calculate(16, 3);
+calculate(5, 9);
+
+// console.log(2 * 4);
+// console.log(4 * 4);
+// console.log(7 * 1);
+// console.log(5 * 3);
+// console.log(5 * 9);
---
+++ theory/objects/index.js
@@ -0,0 +1,11 @@
+// let student = {
+//   name: "Manikant",
+//   roll: 10,
+//   standard: 10,
+//   division: "A",
+// };
+
+// let roll = 10;
+
+// console.log("roll", roll);
+// console.log("student", student.standard, student.roll, student.division);
