519dbdcdcab0bf2568c51d05b402992b696be721
Manikant Jha <manikantcodes@gmail.com>
2024-08-07 10:26:08 +0530

    strings completed

--- theory/strings/pad-start-end.js
+++ theory/strings/pad-start-end.js
@@ -1,14 +1,30 @@
 // 8956231245
 
 const phone = "8956231245";
-const last4Digits = phone.slice(-4);
-const first4Digits = phone.slice(0, 4);
+// const last4Digits = phone.slice(-4);
+// const first4Digits = phone.slice(0, 4);
 
 // const paddedStr = last4Digits.padStart(10, "ijklmnop");
 // const paddedStr = first4Digits.padEnd(10, "x");
 
-for (let i = 1; i <= 100; i++) {
-  console.log(i.toString().padStart(3, "0"));
+// for (let i = 1; i <= 100; i++) {
+//   console.log(i.toString().padStart(3, "0"));
+// }
+
+// console.log("paddedStr", paddedStr);
+
+function padBetween(str, pattern, startingChars = 0, endingChars = 0) {
+  let paddedStr = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if (i < startingChars || i >= str.length - endingChars) {
+      paddedStr += str[i];
+    } else {
+      paddedStr += pattern;
+    }
+  }
+
+  console.log(paddedStr);
 }
 
-console.log("paddedStr", paddedStr);
+padBetween("manikant", "x", 4, 0);
---
+++ theory/strings/template-string.js
@@ -0,0 +1,6 @@
+// const name = "John";
+// const age = 5;
+
+// console.log(name + " is of " + age + " years old.");
+// console.log(`${name} is of ${age} years old.`);
+// console.log(`${name} will be of ${age + 1} years old next year.`);
