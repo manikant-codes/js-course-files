8f0a95ab53fab34d3b1690ffb8a7ece464d18821
Manikant Jha <manikantcodes@gmail.com>
2024-08-08 19:42:02 +0530

    made tasks list

--- projects/tasksList/index.js
+++ projects/tasksList/index.js
@@ -1,21 +1,36 @@
 const taskInput = document.getElementById("task-input");
 const list = document.getElementById("list");
 
-const tasks = [];
+let tasks = [];
+
+function showList() {
+  const html = tasks
+    .map(function (value, index) {
+      return `
+      <li class="list-item">
+        <span>${index + 1}. ${value}</span>
+        <button class="btn-danger" onclick="deleteTask(${index})">Delete</button>
+      </li>
+    `;
+    })
+    .join("");
+
+  list.innerHTML = html;
+}
+
+function deleteTask(deleteIndex) {
+  tasks = tasks.filter(function (value, index, array) {
+    if (index === deleteIndex) {
+      return false;
+    }
+    return true;
+  });
+
+  showList();
+}
 
 function addTask() {
   tasks.push(taskInput.value);
-  const html = tasks
-    .map(function (value) {
-      return (
-        "<li class='list-item'>" +
-        "<span>" +
-        value +
-        "</span>" +
-        "<button>delete</button>" +
-        "</li>"
-      );
-    })
-    .join("");
-  list.innerHTML = html;
+
+  showList();
 }
