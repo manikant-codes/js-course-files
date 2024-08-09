02dac773ff523e5f832dc2b00204a7d3ee6e245a
Manikant Jha <manikantcodes@gmail.com>
2024-08-09 10:31:11 +0530

    js date

--- theory/date/index.js
+++ theory/date/index.js
@@ -1,3 +1,35 @@
-const date = new Date();
+// console.log(new Date("2022-03-21"));
+// console.log(new Date("2022-03"));
+// console.log(new Date("2022"));
+// console.log(new Date("2022-03-21T09:56:45"));
+// console.log(new Date("2022-03-21T09:56:45Z"));
 
-console.log(date);
+console.log(new Date().getFullYear());
+console.log(new Date().getUTCFullYear());
+
+console.log(new Date().getMonth());
+console.log(new Date().getUTCMonth());
+
+console.log(new Date().getMonth());
+console.log(new Date().getUTCMonth());
+
+console.log(new Date().getDate());
+console.log(new Date().getUTCDate());
+
+console.log(new Date().getSeconds());
+console.log(new Date().getUTCSeconds());
+
+// console.log(new Date("25 2015 MARCH"));
+
+// const oneDay = 1000 * 60 * 60 * 24;
+
+// console.log(new Date());
+// console.log(new Date(-oneDay * 40));
+// console.log(new Date(2024, 7));
+// console.log(new Date(9, 7, 9));
+// console.log(new Date(99, 7, 9));
+// console.log(new Date(2024, 7, 9));
+// console.log(new Date(2024, 7, 9, 9));
+// console.log(new Date(2024, 7, 9, 9, 43));
+// console.log(new Date(2024, 7, 9, 9, 43, 50));
+// console.log(new Date(2024, 7, 9, 9, 43, 50, 500));
