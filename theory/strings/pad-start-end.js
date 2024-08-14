ba7e1b0371bd2338f7fcdd92f224ef540dd5e6e7
Manikant Jha <manikantcodes@gmail.com>
2024-08-14 18:44:17 +0530

    pad methods

---
+++ theory/strings/pad-start-end.js
@@ -0,0 +1,9 @@
+const number = "7894561458";
+
+const last3Digits = number.slice(-3);
+const first3Digits = number.slice(0, 3);
+
+// const paddedStr = last3Digits.padStart(10, "*");
+// const paddedStr = first3Digits.padEnd(10, "*");
+
+console.log(paddedStr);
