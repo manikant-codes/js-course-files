061b7b44a9ab15b772678ac9537da4b2f368b45a
Manikant Jha <manikantcodes@gmail.com>
2024-08-16 10:31:44 +0530

    destructuring

---
+++ theory/destructuring/index.js
@@ -0,0 +1,44 @@
+// const obj = {
+//   name: "Ram",
+//   roll: 10,
+//   sciMarks: 45,
+//   mathMarks: 40,
+//   engMarks: 42,
+//   address: {
+//     city: "Surat",
+//     pin: 321654,
+//   },
+// };
+
+// const roll = obj.roll;
+// const sciMarks = obj.sciMarks;
+// const mathMarks = obj.sciMarks;
+// const engMarks = obj.sciMarks;
+
+// const {
+//   roll: rollNumber,
+//   address: { city },
+//   name,
+// } = obj;
+
+// const { city } = address;
+
+// console.log(name);
+// console.log(rollNumber);
+// console.log(sciMarks);
+// console.log(mathMarks);
+// console.log(engMarks);
+// console.log(address);
+// console.log(city);
+
+const arr = ["Ram", 10, { city: "Surat" }];
+
+// const name = arr[0];
+// const roll = arr[1];
+// const city = arr[2].city;
+// const [name, rool, { city }] = arr;
+// console.log(arr);
+
+// console.log(name);
+// console.log(roll);
+// console.log(asd);
