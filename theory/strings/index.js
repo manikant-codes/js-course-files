64d30c03c4c39f278849edcbf331d7ab1a5f8564
Manikant Jha <manikantcodes@gmail.com>
2024-08-17 11:32:25 +0530

    taught string methods

---
+++ theory/strings/index.js
@@ -0,0 +1,42 @@
+// Agar aap ko JS me text ke saath kaam karna hai to aap ko strings banane padenge.
+
+// String banane ke 3 tarike hain.
+
+// "" double quotes:
+// let str = "Ram said, "Hello"."; // Error
+// let str = "Ram said, \"Hello\".";
+// let str = "Ram said, 'Hello'.";
+// console.log("Double Quotes: ", str);
+
+// '' single quotes:
+// let str = 'Ram';
+// let str = 'Ram isn't coming today.'; // Error
+// let str = 'Ram isn\'t coming today.';
+// console.log("Single Quotes: ", str);
+
+// `` back ticks:
+// let str = `Ram`;
+// let str = `Ram `asd `; // Error
+// let str = `Ram \`asd\` `; 
+// let str = `Ram 'asd' `; 
+// let str = `Ram "asd" `; 
+// console.log("Back Ticks: ", str);
+
+// console.log(" Ram \\ ")
+// console.log("Ram\tRam")
+
+// Breaking Long Lines
+// let str = 
+// "Lorem ipsum dolor sit amet consectetur adipisicing elit."  + 
+// " Consectetur culpa repudiandae molestias reiciendis" + 
+// " adipisci at inventore nostrum ipsum accusantium," +
+// " asperiores aliquid ex eveniet impedit ipsa" + 
+// " tempora molestiae aut autem a.";
+
+// let str = `Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+// Eos optio quis obcaecati, vel facere sunt accusantium impedit 
+// iste delectus! Alias recusandae iste eveniet 
+// suscipit aspernatur nihil sapiente 
+// commodi nemo voluptatum?`;
+
+// console.log(str);
