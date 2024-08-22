9f3d3b41c8f35feeb42172077fdc720ed71a3705
Manikant Jha <manikantcodes@gmail.com>
2024-08-22 10:19:41 +0530

    scope and hoisting

--- theory/executionContext/index.js
+++ theory/executionContext/index.js
@@ -1,12 +1,21 @@
 debugger;
 
-var a = 10;
-var b = 20;
+// console.log(a);
+let a = 30;
+let b = 20;
 
-function add(x) {
-  var result = x + b;
-  console.log(result);
+if (true) {
+  if (true) {
+    let a = 10;
+  }
+  let a = 10;
+  let b = 20;
 }
 
-add(a);
-add(5);
+// function add(x) {
+//   var result = x + b;
+//   console.log(result);
+// }
+
+// add(a);
+// add(5);
