07f18825ce5f25c7009c94d68471bbb0b5da298f
Manikant Jha <manikantcodes@gmail.com>
2024-09-02 19:34:19 +0530

    best practices and performance

---
+++ theory/bestPractices/index.js
@@ -0,0 +1,28 @@
+// let a = 10;
+// let b = 20;
+
+// function temp() {
+//   a = 20;
+// }
+
+// temp();
+
+// function add() {
+//   console.log(a + b);
+// }
+
+// add();
+
+// const a = 10;
+// console.log(a);
+
+// const car = { type: "Fiat", model: "500", color: "white" };
+// car = "Fiat";
+
+// console.log(car);
+
+function add(a = 0, b = 0) {
+  console.log(a + b);
+}
+
+add();
