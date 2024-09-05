25f574687bd0d351a2e5b3c7bc4c39760a996897
Manikant Jha <manikantcodes@gmail.com>
2024-09-05 10:27:09 +0530

    modules

---
+++ theory/modules/calc.mjs
@@ -0,0 +1,43 @@
+// export function add(num1, num2) {
+//   return Number(num1) + Number(num2);
+// }
+
+// export function subtract(num1, num2) {
+//   return Number(num1) - Number(num2);
+// }
+
+// export function multiply(num1, num2) {
+//   return Number(num1) * Number(num2);
+// }
+
+// export function divide(num1, num2) {
+//   return Number(num1) / Number(num2);
+// }
+
+// OR
+
+function add(num1, num2) {
+  return Number(num1) + Number(num2);
+}
+
+function subtract(num1, num2) {
+  return Number(num1) - Number(num2);
+}
+
+function multiply(num1, num2) {
+  return Number(num1) * Number(num2);
+}
+
+function divide(num1, num2) {
+  return Number(num1) / Number(num2);
+}
+
+function remainder(num1, num2) {
+  return Number(num1) % Number(num2);
+}
+
+function exponentiation(num1, num2) {
+  return Number(num1) ** Number(num2);
+}
+
+export { add, subtract, multiply, divide };
---
+++ theory/modules/index.mjs
@@ -0,0 +1,36 @@
+const num1Input = document.getElementById("num1");
+const num2Input = document.getElementById("num2");
+const operationSelect = document.getElementById("operation");
+const outputPara = document.getElementById("output");
+const calculateBtn = document.getElementById("calculate");
+
+import { add, subtract, multiply, divide } from "./calc.mjs";
+
+function calculate() {
+  switch (operationSelect.value) {
+    case "add": {
+      const result = add(num1Input.value, num2Input.value);
+      outputPara.innerHTML = result;
+      break;
+    }
+    case "subtract": {
+      const result = subtract(num1Input.value, num2Input.value);
+      outputPara.innerHTML = result;
+      break;
+    }
+    case "multiply": {
+      const result = multiply(num1Input.value, num2Input.value);
+      outputPara.innerHTML = result;
+      break;
+    }
+    case "divide": {
+      const result = divide(num1Input.value, num2Input.value);
+      outputPara.innerHTML = result;
+      break;
+    }
+    default:
+      alert("Invalid operation!");
+  }
+}
+
+calculateBtn.addEventListener("click", calculate);
