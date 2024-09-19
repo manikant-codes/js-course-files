293c2ded4aa09ca999f7eff64fc9716fdfc2cafe
manikant jha <manikantcodes@gmail.com>
2024-09-19 10:14:45 +0530

    taught map

--- theory/map/index.js
+++ theory/map/index.js
@@ -8,9 +8,9 @@
 //   ["roll", 10],
 // ]);
 
-const obj = {};
-obj.name = "ram";
-obj.roll = 10;
+// const obj = {};
+// obj.name = "ram";
+// obj.roll = 10;
 
 const myObj = {};
 
@@ -18,7 +18,23 @@ const map = new Map();
 map.set(myObj, "ram");
 map.set("roll", 10);
 
-console.log("obj", obj);
-console.log("map", map);
+// console.log("obj", obj);
+// console.log("map", map);
 
-console.log("type", map instanceof Array);
+// console.log("type", map instanceof Array);
+
+// console.log("name", map.get({}));
+// console.log("size", map.size);
+// console.log("delete", map.delete("roll"));
+// console.log("clear", map.clear());
+// console.log("has", map.has({}));
+// console.log("keys", Array.from(map.keys()));
+// console.log("values", Array.from(map.values()));
+// console.log("entries", Array.from(map.entries()));
+// map.forEach((value, key, map) => {
+//   //   console.log("value", value);
+//   //   console.log("key", key);
+//   //   console.log("map", map);
+// });
+
+// console.log("map", map);
