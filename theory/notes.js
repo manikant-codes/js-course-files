3f50fa4d68c93544b585521f1141140d5035c9ca
manikant jha <manikantcodes@gmail.com>
2024-09-25 16:53:56 +0530

    taught more js interview questions

--- theory/notes.js
+++ theory/notes.js
@@ -1,3 +1,5 @@
+// "use strict";
+
 // Hoisting:
 // JS ka code 2 parts me chalta hai, pehla part hota hai memory allocation ka aur dusra part hota hai code execution ka.
 // Memeory allocation wale part me aap ne jo variables/functions banaye hai unhe memeory me jagah di jati hai (yane unka declaration kiya jata hai). Isi process ko hoisting kehte hai.
@@ -33,7 +35,7 @@
 // NaN ek special value hai jo JS aap ko tab deta hai jab aap kuch aisa karne ki koshish karo jiska output number naa aaye.
 // E.g.: 2 * "apple"
 
-debugger;
+// debugger;
 
 // if (true) {
 //   console.log(a);
@@ -46,7 +48,88 @@ debugger;
 //   console.log(a);
 // }
 
-(function () {
-  let a = 10;
-  console.log(a);
-})();
+// (function () {
+//   let a = 10;
+//   console.log(a);
+// })();
+
+// pass-by-value vs pass-by-reference
+// let num = 10;
+// let obj = { num: 10 }; // 202
+
+// function doSomething(arg) {
+//   arg.num++;
+//   console.log(arg);
+// }
+
+// doSomething(obj);
+// console.log(obj);
+
+// IIFE (Immediately Invoked Function Expression) / Self Invoking Function
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// console.log(add(2, 2));
+
+// (function (a, b) {
+//   console.log(a + b);
+// })(2, 2);
+
+// function add(a, a) {
+//   console.log(a + a);
+// }
+
+// add(4, 2);
+
+// let a = 10;
+// delete a;
+// console.log(a);
+
+// Higher Order Function:
+// Aise function jo dusre function argument me le ye ya fir andar se koi function return kare unhe Higher Order Function kehte hei.
+// Higher Order Function logic common karne ke kaam me aate hai.
+// array.map(), array.filter(), array.reduce(), etc ye sare Higher Order Functions ke example hai.
+
+// Callback Function:
+// Jo function Higher Order Function ko as an argument pass karte hai unhe callback function kehte hai.
+
+// this:
+// this ek keyword hai jisme us object ka reference hota hai jisne this wala code / this wala function chalaya hoo. Ais sirf normal function ke liye hota hai.
+
+// Arrow function me apni this ki koi value nahi hoti hai, wo this ki value apne parent scope se lete hai.
+
+// const student = {
+//   name: "Ram",
+//   getThis: function () {
+//     const x = () => {
+//       console.log(this);
+//     };
+//     x();
+//   },
+// };
+
+// function add() {
+//   console.log(this);
+// }
+
+// // window.add();
+// student.getThis();
+
+// Currying ek code likhne tarika hai hai jisme ek function andar se dusre functions ko return karte hoo.
+// E.g.:
+
+function add(a, b) {
+  console.log(a + b);
+}
+
+add(2, 2);
+
+function curriedAdd(a) {
+  return function nestedFn(b) {
+    console.log(a + b);
+  };
+}
+
+curriedAdd(2)(4);
