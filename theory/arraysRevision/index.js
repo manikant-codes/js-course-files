01d098308caec37f4e0811f7514218f8dfbba276
manikant jha <manikantcodes@gmail.com>
2024-09-28 12:29:56 +0530

    array revision

---
+++ theory/arraysRevision/index.js
@@ -0,0 +1,21 @@
+// Collection of key:value pairs.
+const obj = {
+  marks: 23,
+  roll: 15,
+  city: {},
+  city: [],
+  city: function () {},
+};
+
+obj.city = "Pune";
+obj[5] = 369852;
+
+// Collection of values.
+const arr = ["Ram", 1, "Surat"];
+arr[arr.length] = 369852;
+arr[arr.length] = "ASD";
+arr[2] = "Pune";
+
+// console.log("obj", obj);
+// console.log("arr", arr);
+console.log("arr", arr);
