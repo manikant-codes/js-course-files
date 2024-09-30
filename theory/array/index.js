89204e9adf79086f423d3e542f77149d9ccc8250
manikant jha <manikantcodes@gmail.com>
2024-09-30 17:27:41 +0530

    array methods

--- theory/array/index.js
+++ theory/array/index.js
@@ -4,33 +4,21 @@
 // object ki keys string hoti hai, in keys ki value aap jo chahe wo (string value) rakh sakte hoo.
 // array ki keys numric hooti hai, ye keys JS automatically generate kardeta hai. Ye keys 0 se start hooti hain.
 
-const obj = {
-  //  0: "Ram",
-  name: "Ram",
-  roll: 10,
-  city: "Surat",
-  address: { city: "Surat" },
-  hobbile: ["singing", "painting", "dancing"],
-  avg: function () {},
-};
+// const obj = {
+//   //  0: "Ram",
+//   name: "Ram",
+//   roll: 10,
+//   city: "Surat",
+//   address: { city: "Surat" },
+//   hobbies: ["singing", "painting", "dancing"],
+//   avg: function () {},
+// };
 
 // console.log(obj.name);
 // console.log(obj["name"]);
 // console.log(obj[0]);
 
-const arr = [
-  "Ram",
-  10,
-  "Surat",
-  true,
-  false,
-  10n,
-  { name: "Ram" },
-  ["singing", "painting", "dancing"],
-  function () {
-    console.log("Hi");
-  },
-];
+// const arr = ["Ram", 10, "Surat", true, false, [], {}, function () {}, new Date(), /abc/];
 
 // const temp = [];
 // temp[0] = "Ram";
@@ -39,5 +27,29 @@ const arr = [
 
 // arr["grade"] = 7.2;
 // console.log(arr.grade)
-arr[1] = 5;
-console.log(arr.toString());
+// arr[1] = 5;
+
+// console.log(arr.toString());
+// console.log(arr.join("-"));
+
+const fruitsList = document.getElementById("fruits-list");
+let temp = "";
+
+// array literal
+const fruits = ["Apple", "Banana", "Cherry", "Date"];
+
+// using new keyword
+// const fruits = new Array("Apple", "Banana", "Cherry", "Date");
+
+// let index = 0;
+
+// while (index < fruits.length) {
+//   console.log(index, fruits[index]);
+//   index++;
+// }
+
+for (let index = 0; index < fruits.length; index++) {
+  temp += "<li>" + fruits[index] + "</li>";
+}
+
+fruitsList.innerHTML = temp;
