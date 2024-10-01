2cae568961e7a13e78b67aeca52890761543fa98
manikant jha <manikantcodes@gmail.com>
2024-10-01 16:24:02 +0530

    search methods

---
+++ theory/array/methods.js
@@ -0,0 +1,13 @@
+const fruits = ["Apple", "Banana", "Cherry", "Date"];
+
+// Is se array me gap reh jata hai. Array ki size change nahi hooti.
+// delete fruits[2];
+
+// splice se array me gap nahi rehta. Array resize hoo jata hai. splice original array ko change karta hai.
+// console.log(fruits.splice(1, 3, "Guava", "Papaya", "Jamun"));
+
+// toSpliced original array ko nahi change karta hai. Naya array return karta hai.
+// console.log(fruits.toSpliced(1, 3, "Guava", "Papaya", "Jamun"));
+
+// console.log(fruits.copyWithin(1, 0, 2));
+// console.log("fruits", fruits);
---
+++ theory/array/searchMethods.js
@@ -0,0 +1,23 @@
+const fruits = ["Date", "Apple", "Cherry", "Banana"];
+
+// console.log(fruits.indexOf("Banana"));
+// console.log(fruits.lastIndexOf("Banana"));
+// console.log(fruits.includes("apple"));
+
+// function add(num1, num2) {
+//   console.log(num1 + num2);
+// }
+
+// add(2, 4);
+
+// const numbers = [4, 9, 16, 25, 29, 8];
+
+// const result = numbers.findLastIndex(function (value, index, array) {
+//   if (value > 18) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// });
+
+// console.log(result);
