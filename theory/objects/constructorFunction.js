2c23f4b2f5486935cc901cab92edc72f6085bc89
manikant jha <manikantcodes@gmail.com>
2024-10-01 10:30:01 +0530

    object prototypes

---
+++ theory/objects/constructorFunction.js
@@ -0,0 +1,73 @@
+// Problems
+// 1. Data Inconsistency, 2. Redundancy, 3. Difficult to Maintain and Work With
+
+// const student1 = {
+//   id: 12345,
+//   fisrtName: "John",
+//   lastName: "Doe",
+//   age: 20,
+//   gender: "Male",
+//   email: "john.doe@example.com",
+//   phone: "123-456-7890",
+//   address: "123 Main St, Anytown, USA",
+//   major: "Computer Science",
+//   gpa: 3.8,
+//   std: 10,
+//   enrolled: true,
+//   scholarship: "Merit-based",
+//   graduationYear: 2025,
+//   clubMembership: "Coding Club",
+//   getFullName: function () {
+//     return this.firstName + " " + this.lastName;
+//   },
+// };
+
+// const student2 = {
+//   id: 12345,
+//   firtsName: "John",
+//   lastName: "Doe",
+//   age: 20,
+//   gender: "Male",
+//   email: "john.doe@example.com",
+//   phone: "123-456-7890",
+//   address: "123 Main St, Anytown, USA",
+//   major: "Computer Science",
+//   gpa: 3.8,
+//   std: 10,
+//   enrolled: true,
+//   scholarship: "Merit-based",
+//   graduationYear: 2025,
+//   clubMembership: "Coding Club",
+//   getFullName: function () {
+//     return this.firstName + " " + this.lastName;
+//   },
+// };
+
+function Student(id, firtsName, lastName) {
+  this.id = id;
+  this.firstName = firtsName;
+  this.lastName = lastName;
+  this.standard = 10;
+  //   this.getFullName = function () {
+  //     return this.firstName + " " + this.lastName;
+  //   };
+}
+
+Student.prototype.city = "Surat";
+
+Student.prototype.getFullName = function () {
+  return this.firstName + " " + this.lastName;
+};
+
+const stud1 = new Student(1, "Rahul", "Doe", 10);
+
+const stud2 = new Student(2, "Priya", "Singh", 10);
+
+Array.prototype.map = undefined;
+
+const arr = [];
+
+console.log(arr.map);
+
+console.log(stud1);
+console.log(stud2.city);
--- theory/objects/intro.js
+++ theory/objects/intro.js
@@ -12,10 +12,11 @@ const studentRoll = 10;
 const studentCity = "Surat";
 
 // Object literal
-// const student = {
-//   name: "Ram",
-//   roll: 10,
-// };
+const student = {
+  name: "Ram",
+  roll: 10,
+  city: "Surat",
+};
 // student.city = "Surat";
 
 // new keyword ka use karke
@@ -25,70 +26,3 @@ const studentCity = "Surat";
 // const student = new Object({ name: "Ram" });
 // student.roll = 10;
 // console.log(student);
-
-// Problems
-// 1. Data Inconsistency, 2. Redundancy, 3. Difficult to Maintail and Work With
-
-// const student1 = {
-//   id: 12345,
-//   firstName: "John",
-//   lastName: "Doe",
-//   age: 20,
-//   gender: "Male",
-//   email: "john.doe@example.com",
-//   phone: "123-456-7890",
-//   address: "123 Main St, Anytown, USA",
-//   major: "Computer Science",
-//   gpa: 3.8,
-//   std: 10,
-//   enrolled: true,
-//   scholarship: "Merit-based",
-//   graduationYear: 2025,
-//   clubMembership: "Coding Club",
-//   getFullName: function () {
-//     return this.firstName + " " + this.lastName;
-//   },
-// };
-
-// const student2 = {
-//   id: 12345,
-//   firtsName: "John",
-//   lastName: "Doe",
-//   age: 20,
-//   gender: "Male",
-//   email: "john.doe@example.com",
-//   phone: "123-456-7890",
-//   address: "123 Main St, Anytown, USA",
-//   major: "Computer Science",
-//   gpa: 3.8,
-//   std: 10,
-//   enrolled: true,
-//   scholarship: "Merit-based",
-//   graduationYear: 2025,
-//   clubMembership: "Coding Club",
-//   getFullName: function () {
-//     return this.firstName + " " + this.lastName;
-//   },
-// };
-
-function Student(id, firstName, lastName) {
-  this.id = id;
-  this.firstName = firstName;
-  this.lastName = lastName;
-  //   this.standard = 10;
-  //   this.getFullName = function () {
-  //     return this.firstName + "-" + this.lastName;
-  //   };
-}
-
-Student.prototype.standard = 10;
-
-Student.prototype.getFullName = function () {
-  return this.firstName + "-" + this.lastName;
-};
-
-const student1 = new Student(1, "John", "Doe");
-const student2 = new Student(1, "James", "Doe");
-
-console.log(student1);
-console.log(student2);
