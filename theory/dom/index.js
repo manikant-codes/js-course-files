199936b6a4c2b4301a892cc399461a839409eaae
Manikant Jha <manikantcodes@gmail.com>
2024-10-11 10:42:11 +0530

    dom

--- theory/dom/index.js
+++ theory/dom/index.js
@@ -1,10 +1,33 @@
-const byId = document.getElementById("my-para");
-const byTagName = document.getElementsByTagName("p");
-const byClassName = document.getElementsByClassName("para");
+// --------------------------------------------------
+// getElement/s
+// --------------------------------------------------
+// const byId = document.getElementById("my-para");
+// const byClassName = document.getElementsByClassName("para");
+// const byTagName = document.getElementsByTagName("p");
 
+// --------------------------------------------------
+// querySelector/querySelectorAll
+// --------------------------------------------------
+// const byId = document.querySelector("#my-para");
+// const byClassName = document.querySelectorAll(".para");
+// const byTagName = document.querySelectorAll("p");
+// const byElementClassName = document.querySelectorAll("p.para");
+// const byFirstElement = document.querySelectorAll("p:first-child");
+
+// console.log("byId", byId);
+// console.log("byClassName", byClassName);
+// console.log("byTagName", byTagName);
+// console.log("byFirstElement", byFirstElement);
+
+// --------------------------------------------------
+// innerText/innerHTML
+// --------------------------------------------------
 // byId.innerText = "<span>New Content</span>";
 // byId.innerHTML = "<span style='color: red'>New Content</span>";
 
+// --------------------------------------------------
+// Select/Change Attributes
+// --------------------------------------------------
 // byId.className = "red";
 // byId.id = "my-new-para";
 // byId.style.color = "green";
@@ -13,6 +36,27 @@ const byClassName = document.getElementsByClassName("para");
 // byId.setAttribute("id", "my-new-para");
 // byId.setAttribute("style", "color: green");
 
-console.log("myPara", byId.innerText);
-console.log("allParas", byTagName);
-console.log("classParas", byClassName);
+// --------------------------------------------------
+// Create Element
+// --------------------------------------------------
+const paragraphNode = document.createElement("p");
+// para.innerText = "Hello World!";
+// para.innerHTML = "<span class='red'>Hello World!</span>";
+const paragraphTextNode = document.createTextNode("Hello World!");
+paragraphNode.appendChild(paragraphTextNode);
+paragraphNode.className = "red";
+
+const buttonNode = document.createElement("button");
+const buttonTextNode = document.createTextNode("Add Paragraph");
+buttonNode.appendChild(buttonTextNode);
+buttonNode.onclick = function () {
+  const paragraphNode = document.createElement("p");
+  const paragraphTextNode = document.createTextNode("Hello World!");
+  paragraphNode.appendChild(paragraphTextNode);
+  document.body.appendChild(paragraphNode);
+};
+
+document.body.appendChild(paragraphNode);
+document.body.appendChild(buttonNode);
+
+console.log(paragraphNode.innerHTML);
