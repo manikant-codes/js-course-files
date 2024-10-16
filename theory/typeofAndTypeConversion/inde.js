62eb302edcc86149e5e7544fea23c78e3127f58e
Manikant Jha <manikantcodes@gmail.com>
2024-10-16 15:59:41 +0530

    typeof and type conversion

---
+++ theory/typeofAndTypeConversion/inde.js
@@ -0,0 +1,53 @@
+// console.log(typeof 10);
+// console.log(typeof "ram");
+// console.log(typeof true);
+// console.log(typeof 10n);
+// console.log(typeof undefined);
+// console.log(typeof null);
+// console.log(typeof Symbol("variable"));
+
+// console.log(typeof {});
+// console.log(typeof []);
+// console.log(typeof new Date());
+// console.log(typeof /asd/);
+// console.log(typeof new Set());
+// console.log(typeof function () {});
+// console.log(typeof new Map());
+
+// console.log({} instanceof Array);
+// console.log([] instanceof Array);
+// console.log(new Date() instanceof Date);
+// console.log(new Set() instanceof Set);
+// console.log(new Map() instanceof Map);
+// console.log(function () {} instanceof Function);
+// console.log(/asd/ instanceof RegExp);
+
+// console.log([] instanceof Array);
+// console.log(Array.isArray({}));
+
+// console.log(typeof typeof undefined);
+
+// console.log("" == 0);
+// console.log(null === undefined);
+
+// console.log([].constructor);
+// console.log({}.constructor);
+// console.log(/asd/.constructor);
+// console.log(function () {}.constructor);
+
+// console.log(Boolean(null));
+// console.log(!!null);
+
+// console.log(Number(false));
+// console.log(parseInt("3.5"));
+// console.log(parseFloat("3.5"));
+// console.log(+false);
+
+// console.log(String(false));
+// console.log((false).toString());
+// false + ""
+
+console.log(Number(new Date()));
+console.log(new Date().getTime());
+console.log(String(new Date()));
+console.log(new Date().toString());
