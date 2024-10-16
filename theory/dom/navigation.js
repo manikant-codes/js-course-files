0aae477b10d9a612b8c1b9d98ed7ae76943226a7
Manikant Jha <manikantcodes@gmail.com>
2024-10-16 14:14:46 +0530

    dom navigation

---
+++ theory/dom/navigation.js
@@ -0,0 +1,5 @@
+const childDiv2 = document.getElementById("child-div-2");
+
+// console.log((childDiv1.parentElement.style.backgroundColor = "orange"));
+// console.log(childDiv2.children[0].nodeType);
+// console.log(childDiv2.nextElementSibling);
