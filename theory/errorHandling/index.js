ff84b8ad3d4d7c170b8c4e694219b8084b928160
Manikant Jha <manikantcodes@gmail.com>
2024-10-18 15:38:25 +0530

    error handling

---
+++ theory/errorHandling/index.js
@@ -0,0 +1,47 @@
+// const students = undefined;
+
+// [
+//   { id: 1, fname: "Ram", lname: "Das" },
+//   { id: 2, fname: "Manu", lname: "Kumar" }
+// ];
+
+// try {
+//   for (const value of students) {
+//     console.log(value.fname + " " + value.lname);
+//   }
+//   console.log("Statement 1");
+//   console.log("Statement 2");
+//   console.log("Statement 3");
+// } catch (error) {
+//   //   console.log("error name", error.name);
+//   //   console.log("error message", error.message);
+//   //   alert("Something went wrong!");
+// }
+
+// const num1 = 5;
+// const num2 = 2;
+
+// try {
+//   if (num2 === 0) {
+//     throw new Error("Can't divede by 0!");
+//   } else {
+//     console.log(num1 / num2);
+//   }
+// } catch (error) {
+//   console.log(error);
+//   alert(error.message);
+// } finally {
+//   alert("Calculation completed!");
+// }
+
+// console.log("Task 1");
+// console.log("Task 2");
+// console.log("Task 3");
+// console.log("...");
+// console.log("Task 100");
+
+// const num === 4;
+
+// console.log(num);
+
+console.log((10.2356).toPrecision(500));
