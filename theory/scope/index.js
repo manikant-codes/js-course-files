380c4af797551efd807aa5f68a68387a55ace61a
Manikant Jha <manikantcodes@gmail.com>
2024-10-22 18:29:25 +0530

    extra revision

--- theory/scope/index.js
+++ theory/scope/index.js
@@ -1,4 +1,6 @@
-debugger;
+// "use strict";
+
+// debugger;
 
 // Scope ek jagah hai jaha pe us execution context ke variables aur functions rakhe jaate hai  jaha code chal raha hoo. Is jagah (scope) pe se decide hota hai ke yaha rakhe variables ya functions kaha milenge kaha nahi.
 
@@ -6,17 +8,83 @@ debugger;
 
 // Hoisting ek process hai jime code chale us se pehle variables/functions ko memory me jagah mil jaati hai.
 
-const a = 10;
-const b = 20;
+// const a = 10;
+// const b = 20;
 
-if (true) {
-  const msg = "Correct";
-}
+// if (true) {
+//   const msg = "Correct";
+// }
 
-function add() {
-  const sum = a + b;
-  console.log(sum);
-}
+// function add() {
+//   const sum = a + b;
+//   console.log(sum);
+// }
 
-add();
-console.log("sum", sum);
+// add();
+
+// console.log("sum", sum);
+
+// const obj = {
+//   name: "Ram",
+//   getThis: function () {
+//     console.log("this", this);
+//   }
+// };
+
+// obj.getThis();
+
+// function getThis() {
+//   console.log("this", this);
+// }
+
+// window.getThis();
+
+// (function add(a, b) {
+//   console.log(a + b);
+// })(2, 4);
+
+// console.log(add());
+
+// function add(a, b) {
+//   console.log(a + b);
+// }
+
+// add(2, 4);
+
+// function add(a) {
+//   return function (b) {
+//     console.log(a + b);
+//   };
+// }
+
+// add(2)(4);
+
+// function add(num) {
+//   if (num < 20) {
+//     return add(num + 1);
+//   }
+//   return num;
+// }
+
+// const num = add(1);
+
+// console.log(num);
+
+// function add() {
+//   return 2 + 2;
+// }
+
+// const add = function () {
+//   return 2 + 2;
+// };
+
+// const add = () => {
+//   return 2 + 2;
+// };
+
+const student = { name: "Ram", roll: 10 };
+
+// const name = student.name;
+// const roll = student.roll;
+// OR
+const { name, roll } = student;
