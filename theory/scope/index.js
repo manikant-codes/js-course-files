ba06f92a64ddf154ae3d26d3c10b4e432fdd84f5
Manikant Jha <manikantcodes@gmail.com>
2024-10-22 15:09:27 +0530

    scope and hoisting

--- theory/scope/index.js
+++ theory/scope/index.js
@@ -1,21 +1,22 @@
+debugger;
+
+// Scope ek jagah hai jaha pe us execution context ke variables aur functions rakhe jaate hai  jaha code chal raha hoo. Is jagah (scope) pe se decide hota hai ke yaha rakhe variables ya functions kaha milenge kaha nahi.
+
+// Scope-chaining ek process hai jisme child scope se parent scope ke variables ya functions access kiye jate hai.
+
+// Hoisting ek process hai jime code chale us se pehle variables/functions ko memory me jagah mil jaati hai.
+
 const a = 10;
 const b = 20;
 
+if (true) {
+  const msg = "Correct";
+}
+
 function add() {
-  return a + b;
+  const sum = a + b;
+  console.log(sum);
 }
 
-function multiply() {
-  const x = 5;
-  const y = 2;
-  return x * y;
-}
-
-console.log("x", x);
-console.log("y", y);
-
-const sum = add();
-const product = multiply();
-
+add();
 console.log("sum", sum);
-console.log("product", multiply());
