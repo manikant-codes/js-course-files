8ec36e8713b60a8a33d94848558b4801c7fb1ebf
Manikant Jha <manikantcodes@gmail.com>
2024-10-24 09:45:22 +0530

    objects completed

--- theory/object/getterSetters.js
+++ theory/object/getterSetters.js
@@ -1,21 +1,29 @@
 // getter ek method/function hai jo object ke bante waqt apne naam ki ek nayi property object me add kardeta hai, iski value jo aap us getter function se return kar rahe hoo wo hoo gi.
 
+// getter function aap ko apne object me calculated property add karne deta hai.
+
 // Is getter ki banayi property ko directly nahi change kar sakte hai.
 
 const student = {
   fname: "Ritu",
   lname: "Kumar",
-  //   getFullName: function () {
-  //     return this.fname + " " + this.lname;
-  //   }
+  // getFullName: function () {
+  //   return this.fname + " " + this.lname;
+  // },
   get fullName() {
     return this.fname + " " + this.lname;
+  },
+  set fullName(arg) {
+    this.fname = arg.fname;
+    this.lname = arg.lname;
   }
 };
 
-student.fullName = "Kuch Aur";
-
-console.log("student", student);
-
+// console.log(student.fname + " " + student.lname);
 // console.log(student.getFullName());
+console.log(student);
+
+// student.fullName = "Akshay Kumar";
+student.fullName = { fname: "Akshay", lname: "Kumar" };
+
 console.log(student.fullName);
